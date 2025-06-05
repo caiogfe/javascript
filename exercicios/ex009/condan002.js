893b56af9ac0e70a796ba4081e72866b18ddbc78
caiogfe <130512314+caiogfe@users.noreply.github.com>
2025-06-05 19:03:32 -0300

    finalizei exercicios de condição aninhada

---
+++ exercicios/ex009/condan002.js
@@ -0,0 +1,12 @@
+var atual = new Date()
+var hora = atual.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora <= 4) {
+    console.log('Boa madrugada!')
+} else if (hora < 12) {
+    console.log('Bom dia!')
+} else if (hora <= 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
