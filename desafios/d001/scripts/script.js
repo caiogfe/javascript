c6e65d592e1466531e732c9b072be04a31e7e8a1
caiogfe <130512314+caiogfe@users.noreply.github.com>
2025-06-07 15:24:06 -0300

    finalizei d001

---
+++ desafios/d001/scripts/script.js
@@ -0,0 +1,17 @@
+function carregar() {
+    var msg = document.querySelector('div#msg')
+    var img = document.querySelector('img#paisagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são <strong>${hora} horas</strong>.`
+    if (hora >= 0 && hora < 12) {
+        img.src = "imagens/manha.png"
+        document.body.style.background = '#ebe9b9'
+    } else if (hora >= 12 && hora < 19) {
+        img.src = "imagens/tarde.png"
+        document.body.style.background = '#f0845d'
+    } else {
+        img.src = "imagens/noite.png"
+        document.body.style.background = '#03173d'
+    }
+}
