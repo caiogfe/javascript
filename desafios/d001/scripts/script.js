81d06679f6da90fc3c4b1e926b487b8f0d5a9bca
caiogfe <130512314+caiogfe@users.noreply.github.com>
2025-06-07 15:33:21 -0300

    atualizei o footer com js

--- desafios/d001/scripts/script.js
+++ desafios/d001/scripts/script.js
@@ -7,11 +7,14 @@ function carregar() {
     if (hora >= 0 && hora < 12) {
         img.src = "imagens/manha.png"
         document.body.style.background = '#ebe9b9'
+        document.getElementById('rodape').style.backgroundColor = '#747358'
     } else if (hora >= 12 && hora < 19) {
         img.src = "imagens/tarde.png"
         document.body.style.background = '#f0845d'
+        document.getElementById('rodape').style.backgroundColor = ('#703b28')
     } else {
         img.src = "imagens/noite.png"
         document.body.style.background = '#03173d'
+        document.getElementById('rodape').style.backgroundColor = ('#010a1b')
     }
 }
