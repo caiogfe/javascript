07bc808cc3dd9215954d36b7f38075120fabc5c2
caiogfe <130512314+caiogfe@users.noreply.github.com>
2025-06-11 19:50:06 -0300

    finalizei d003

--- desafios/d003/scripts/script.js
+++ desafios/d003/scripts/script.js
@@ -5,15 +5,26 @@ function contar() {
     let res = document.querySelector('p#res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0)  {
-        window.alert('[ERRO] Estão faltando dados!')
+        res.innerHTML = ('Impossível contar!')
     } else {
-        res.innerHTML = 'Contando: '    
+        res.innerHTML = 'Contando: <br>'    
         let i = Number(ini.value)    
         let f = Number(fim.value)
         let p = Number(pas.value)
-
-        for(let cont = i; cont <= f; cont += p) {
-            res.innerHTML += `${cont} \u{1F449}`
+        if (p <= 0) {
+            window.alert('[ERRO] Passo Invalído! Considerando PASSO 1.')
+            p = 1
+        }
+        if ( i < f ) {
+            // Contagem crescente
+            for(let cont = i; cont <= f; cont += p) {
+                res.innerHTML += ` ${cont} \u{1F449}`
+            }
+        } else {
+            // Contagem regressiva
+            for(let cont = i; cont >= f; cont -= p) {
+                res.innerHTML += ` ${cont} \u{1F449}`
+            }
         }
         res.innerHTML += `\u{1F3C1}`
     }
